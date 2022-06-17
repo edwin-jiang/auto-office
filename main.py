c02f3f66f76c2a017a9ddd5a151d7ff8e3dec403
edwin-jiang <26100690@qq.com>
2022-06-18 04:44:22 +0800

    添加操作界面

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
+import time
+import PySimpleGUI as sg
 from openpyxl import Workbook,load_workbook
 import pandas as pd
 
+#操作区分
+optype = 0
+# 主题色
+sg.theme('DarkAmber')
+# 窗口布局
+layout = [
+    [sg.Text('文档操作小工具 ver0.1')],
+    [sg.Multiline('', size=(100,10), key='textContent')],
+    [sg.Text('操作选项')],
+    [sg.Radio('创建Excel', 'S1', enable_events=True, key='id0', default=True),
+     sg.Radio('读取Excel', 'S1', enable_events=True, key='id1'),],
+    [sg.Button('执行', key='execBtn'),]
+]
 
 def createExcelFile():
     # 创建一个工作簿对象
@@ -14,14 +29,38 @@ def createExcelFile():
     # 最后关闭文件
     wb.close()
 
-def openExcelFile():
+def openExcelFile(values):
+    filename = 'Mytest.xlsx'
     # 加载工作簿
-    wb2 = load_workbook('Mytest.xlsx')
+    wb = load_workbook(filename)
     # 获取sheet页
-    ws2 = wb2['mytest']
-    ws3 = wb2.get_sheet_by_name('mytest')
+    ws = wb['mytest']
+    ws.cell(row=1,column=1).value = values
     # 打印sheet页的颜色属性值
-    print('color:',ws2.sheet_properties.tabColor)
-    wb2.close()
+    print('color:',ws.sheet_properties.tabColor)
+    wb.save(filename)
+    wb.close()
 
-openExcelFile()
+# 创建窗口
+window = sg.Window('文档操作小工具 ver0.1', layout)
+
+# 循环处理事件
+while True:
+    event, values = window.read()
+
+    # 用户点击X关闭窗口或点击退出按钮
+    if event == sg.WIN_CLOSED:
+        break
+    if event == 'id0':
+        optype = 0
+    if event == 'id1':
+        optype = 1
+    print(optype)
+
+    if event == 'execBtn':
+        if optype == 0:
+            createExcelFile()
+        else:
+            openExcelFile(values['textContent'])
+
+window.close()
