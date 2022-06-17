9ae9e396c42d42598bea2e9033792b586eb8a5dc
edwin-jiang <26100690@qq.com>
2022-06-18 03:58:41 +0800

    初期化中

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # auto-office
 Excel and word and PowerPoint automation
+ただoffice関連ファイルの操作ではなく、資料作成など関連フロー、承認など、作業配りの機能実装も目的になっていると予定してある
--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-import openpyxl
+from openpyxl import Workbook,load_workbook
+import pandas as pd
+
+
+def createExcelFile():
+    # 创建一个工作簿对象
+    wb = Workbook()
+    # 在索引为0的位置创建一个名为mytest的sheet页
+    ws = wb.create_sheet('mytest',0)
+    # 对sheet页设置一个颜色（16位的RGB颜色）
+    ws.sheet_properties.tabColor = 'ff72BA'
+    # 将创建的工作簿保存为Mytest.xlsx
+    wb.save('Mytest.xlsx')
+    # 最后关闭文件
+    wb.close()
+
+def openExcelFile():
+    # 加载工作簿
+    wb2 = load_workbook('Mytest.xlsx')
+    # 获取sheet页
+    ws2 = wb2['mytest']
+    ws3 = wb2.get_sheet_by_name('mytest')
+    # 打印sheet页的颜色属性值
+    print('color:',ws2.sheet_properties.tabColor)
+    wb2.close()
+
+openExcelFile()
